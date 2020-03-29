baa56352f20e254d59400ea0a0cfc5c688b92420
ehmicky <ehmicky@gmail.com>
2020-03-29 21:48:39 +0200

    Fix test

--- test/main.js
+++ test/main.js
@@ -9,6 +9,8 @@ test('Smoke test', async (t) => {
   const { exitCode, stdout } = await execa(await BINARY_PATH, [TASKS_FILE], {
     reject: false,
   })
-  const stdoutA = stdout.replace(/User-Agent.*/u, '')
+  const stdoutA = stdout
+    .replace(/User-Agent.*/gu, '')
+    .replace(/ [^ :]+:80/gu, '')
   t.snapshot({ exitCode, stdout: stdoutA })
 })
