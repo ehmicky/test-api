c4f68f014671e99f02fc75e858189cb93829c4fa
ehmicky <ehmicky@gmail.com>
2021-06-13 19:33:24 +0200

    Fix tests on Windows

--- test/main.js
+++ test/main.js
@@ -1,14 +1,17 @@
+import { relative } from 'path'
 import { fileURLToPath } from 'url'
 
 import test from 'ava'
 import execa from 'execa'
 import { getBinPath } from 'get-bin-path'
+import unixify from 'unixify'
 
 const TASKS_FILE = fileURLToPath(new URL('tasks.yml', import.meta.url))
+const TASKS_GLOB = unixify(relative('.', TASKS_FILE))
 
 test('Smoke test', async (t) => {
   const binaryPath = await getBinPath()
-  const { exitCode, stdout } = await execa('node', [binaryPath, TASKS_FILE], {
+  const { exitCode, stdout } = await execa('node', [binaryPath, TASKS_GLOB], {
     reject: false,
   })
   const stdoutA = stdout
