d50373082c5716f67a957ca8556174664da87c44
ehmicky <ehmicky@gmail.com>
2022-02-06 13:28:37 +0100

    Fix new version of `dot-prop`

--- src/core/spec/run/invalid.js
+++ src/core/spec/run/invalid.js
@@ -1,6 +1,10 @@
-import dotProp from 'dot-prop'
+import { getProperty, setProperty } from 'dot-prop'
 
-// Inverse OpenAPI params where `call.*: invalid` was used
+// Inverse OpenAPI params where `call.*: invalid` was used.
+// If cannot find OpenAPI definition, set as an `anything` schema.
+// TODO: json-schema-faker support for the `not` keyword is lacking
+// E.g. `type` array is not supported, so `invalid value` actually does not
+// work at the moment.
 export const setInvalidParams = function ({
   params,
   specialValues: { invalid },
@@ -9,24 +13,7 @@ export const setInvalidParams = function ({
 }
 
 const reduceInvalidParam = function (params, path) {
-  // Retrieve current OpenAPI definition
-  const param = dotProp.get(params, path)
-
-  // Then inverse it
-  const paramA = getInvalidParam({ param })
-
-  // And set it back
-  return dotProp.set(params, path, paramA)
-}
-
-const getInvalidParam = function ({ param }) {
-  // If cannot find OpenAPI definition, set as an `anything` schema
-  if (param === undefined) {
-    return {}
-  }
-
-  // TODO: json-schema-faker support for the `not` keyword is lacking
-  // E.g. `type` array is not supported, so `invalid value` actually does not
-  // work at the moment.
-  return { not: param }
+  const param = getProperty(params, path)
+  const invertedParam = param === undefined ? {} : { not: param }
+  return setProperty(params, path, invertedParam)
 }
