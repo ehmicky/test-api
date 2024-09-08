bf57296c0407f13cefd4d7cb62af0702b6915f8f
ehmicky <ehmicky@gmail.com>
2024-09-08 19:31:18 +0100

    Use nano-spawn

--- src/main.test.js
+++ src/main.test.js
@@ -2,18 +2,18 @@ import { relative } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 import test from 'ava'
-import { execaNode } from 'execa'
+import spawn from 'nano-spawn'
 import { getBinPath } from 'get-bin-path'
 import unixify from 'unixify'
 
 const TASKS_FILE = fileURLToPath(new URL('tasks.yml', import.meta.url))
 const TASKS_GLOB = unixify(relative('.', TASKS_FILE))
 
+const BIN_PATH = getBinPath()
+
 test('Smoke test', async (t) => {
-  const { exitCode, stdout } = await execaNode(
-    await getBinPath(),
-    [TASKS_GLOB],
-    { reject: false },
+  const { exitCode, stdout } = await t.throwsAsync(
+    spawn('node', [await BIN_PATH, TASKS_GLOB]),
   )
   const stdoutA = stdout
     .replaceAll(/User-Agent.*/gu, '')
